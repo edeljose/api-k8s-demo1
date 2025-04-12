33e0a49625c384dfc71b735251839350cfb3ce9c
Edel Amaya <edeljose@gmail.com>
2025-04-12 09:43:53 -0500

    test fail

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/ping', (req, res) => {
-  res.json({ message: 'pong' });
+  res.json({ message: 'pong fail' });
 });
 
 app.listen(port, () => {
