3e6896e075af6934e4efa8346c774d583e6f299c
Edel Amaya <edeljose@gmail.com>
2025-04-12 11:44:34 -0500

    Test con EsLint success 2

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/ping', (req, res) => {
-  res.json({ message: 'pong1' }); // pong1 es intencional para que falle el test
+  res.json({ message: 'pong1'   }); // o cualquier cosa que quieras probar
 });
 
 if (require.main === module) {
   app.listen(port, () => {
-    console.log(`API corriendo en http://localhost:${port}`);
+    console.log(API corriendo en http://localhost:${port});
   });
 }
 
-module.exports = app;
-
+module.exports = app; // <-- exportas la app para usarla en tests
